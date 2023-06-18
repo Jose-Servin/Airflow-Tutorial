f060359fc056feaad6c314cd610cc131a1363448
Jose Servin <Servinjose96@gmail.com>
2023-06-17 22:03:21 -0500

    set up postgres connector in Airflow

--- dags/dag_with_postgres.py
+++ dags/dag_with_postgres.py
@@ -11,24 +11,24 @@ default_args = {
 }
 
 with DAG(
-    dag_id="Postgres_airflow_connection_test_v1",
+    dag_id="Postgres_airflow_connection_test_v01",
     default_args=default_args,
     description="First Airflow with Postgres connection",
-    start_date=datetime(2023, 6, 16),
-    schedule_interval="@daily"
+    start_date=datetime(2023, 6, 17)
 ) as dag:
 
-    create_pet_table = PostgresOperator(
+    create_weather_table = PostgresOperator(
         task_id="create_pet_table",
         postgres_conn_id="postgres_localhost",
         sql="""
-            CREATE TABLE IF NOT EXISTS pet (
-            pet_id SERIAL PRIMARY KEY,
-            name VARCHAR NOT NULL,
-            pet_type VARCHAR NOT NULL,
-            birth_date DATE NOT NULL,
-            OWNER VARCHAR NOT NULL);
-          """,
+            CREATE TABLE IF NOT EXISTS master_weather (
+            weather_id SERIAL PRIMARY KEY,
+            forecast_date DATE NOT NULL,
+            city VARCHAR NOT NULL,
+            state VARCHAR NOT NULL,
+            max_temp INTEGER NOT NULL,
+            min_temp INTEGER NOT NULL);
+          """
     )
 
-    create_pet_table
+    create_weather_table
